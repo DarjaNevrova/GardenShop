f79402fa7247e5a961f8f87f4e68b93cfaa6762e
darya_2k9l82o <daryan020298@gmail.com>
2023-11-29 16:33:26 +0200

    MyLastCommit

--- src/pages/AllProductsPage/index.jsx
+++ src/pages/AllProductsPage/index.jsx
@@ -4,22 +4,13 @@ import AllProductsItem from '../../components/AllProductsItem';
 import style from './style.module.css';
 import ProductsFilters from '../../components/ProductsFilters';
 import Container from '../../UI/Container';
-import NotFound from '../../images/NotFound.webp';
 
 export default function AllProductsPage() {
 
   const productsWithDiscounts = useCalculate();
-
   const result = productsWithDiscounts.filter(({ show }) => Object.values(show).every(elem => elem))
 
-  if (result.length === 0) {
-    return (
-      <div className={style.notFound}>
-        <h1>Sorry, the product was not found.</h1>
-        <img src={NotFound} alt={NotFound} />
-      </div>
-    )
-  }
+  
   return (
     <Container>
       <h1 className={style.paragraph}>All products</h1>
